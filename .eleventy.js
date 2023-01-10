7950c8e7374514ccd221c13e5d9762fe444a094b
utopia58 <110259900+utopia58@users.noreply.github.com>
2023-01-10 18:56:26 -0500

    11tyfied static html

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = function(eleventyConfig) {
+
+    const { DateTime } = require("luxon");
+
+    eleventyConfig.addPassthroughCopy('./src/style.css');
+    eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addFilter('postDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+})
+
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    };
+}
